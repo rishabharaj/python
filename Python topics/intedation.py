c3dfcc0dfd638bda2afaed81f8ac78844130e53c
Rishabharaj Sharma <137804352+rishabharaj@users.noreply.github.com>
2025-05-01 14:41:17 +0530

    Update intedation.py

--- Python topics/intedation.py
+++ Python topics/intedation.py
@@ -1,12 +1,13 @@
 if 5 > 2:
-  print("Five is greater than two!")
+  print("Five is greater than two! first condiiton")
 
-  #Atleast ek ya fir jitna chaho space hona chahiye starting me jisko indetation kehte hai
+  #Atleast ek tab ya four  space hona chahiye jisko indetation kehte hai
   # indetation se hi block ko pehchanta hai
 if 5 > 2:
-print("Five is greater than two!") # nhi hoga space to ese hi 
+print("Five is greater than two! second condition") # nhi hoga space to ese hi 
 
 #same number of space use krna hoga warna error dega
 if 5 > 2:
- print("Five is greater than two!")
-        print("Five is greater than two!")
+ print("Five is greater than two! third condition part 1")
+        print("Five is greater than two! third condition part 2") 
+# third condition part2 will give indentation error
